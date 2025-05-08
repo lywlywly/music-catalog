47692ede99e8e5c2bb142943c4b86a719ed66ed6
Luyao Wang <lywlywly@gmail.com>
2025-05-08 01:32:09 -0700

    use o2 optimization

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,4 +1,5 @@
 rm -rf build && mkdir build && cd build
-cmake .. -DICU_ROOT="$(brew --prefix icu4c)"
+# cmake .. -DICU_ROOT="$(brew --prefix icu4c)" -DCMAKE_BUILD_TYPE=RelWithDebInfo
 # cmake .. -DICU_ROOT="$(brew --prefix icu4c)" -DCMAKE_PREFIX_PATH="$(brew --prefix)"
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make
